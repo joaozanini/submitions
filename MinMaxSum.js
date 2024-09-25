8cd8edd1ada16ad9bea49d2da9af0dd238030ab2
João Vitor <joaozanini.profissional@gmail.com>
2024-09-24 23:31:28 -0300

    Update MinMaxSum.js

--- MinMaxSum.js
+++ MinMaxSum.js
@@ -5,11 +5,11 @@ function miniMaxSum(arr) {
     
     for (let i = 0; i < arr.length - 1; i++) {
         min += arr[i];
-    }
+    };
     
     for (let i = 1; i < arr.length; i++) {
         max += arr[i];
-    }
+    };
 
     console.log(min, max);
 }

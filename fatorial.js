c6e0dff0c98da2984c605f210e74ee810128f865
João Vitor <joaozanini.profissional@gmail.com>
2024-09-24 21:33:05 -0300

    Create fatorial.js

---
+++ fatorial.js
@@ -0,0 +1,9 @@
+function fatorial(n) {
+    let resultado = 1;
+    for (let i = 2; i <= n; i++) {
+      resultado *= i;
+    }
+    return resultado;
+  }
+  
+  console.log(fatorial(5)); // Saída: 120
